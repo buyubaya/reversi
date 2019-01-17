428124be2ff6606ba32944f1156986338a8faa49
Le <hle69@vdc.csc.com>
2019-01-17 17:17:57 +0700

    Updates

---
+++ precache-manifest.cb72cceac61f02ab263fd2fba2067113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "12ca339ff6a689be46ec",
+    "url": "/reversi/static/js/runtime~main.12ca339f.js"
+  },
+  {
+    "revision": "f4cee5e2a5b6eb885fe9",
+    "url": "/reversi/static/js/main.f4cee5e2.chunk.js"
+  },
+  {
+    "revision": "40074e560ea9c7f1f7ed",
+    "url": "/reversi/static/js/1.40074e56.chunk.js"
+  },
+  {
+    "revision": "f4cee5e2a5b6eb885fe9",
+    "url": "/reversi/static/css/main.3116c4b8.chunk.css"
+  },
+  {
+    "revision": "27a38c21cb71f6daef9240c84b899b11",
+    "url": "/reversi/index.html"
+  }
+];
